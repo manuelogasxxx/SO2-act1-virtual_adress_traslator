4b59bafeabb04e36160e78700f58f2c5eb42c604
manuelogasxxx <manolito2500604@gmail.com>
2025-02-05 17:00:03 -0600

    "gitignore" + start

--- adress_traslator.rs
+++ adress_traslator.rs
@@ -1,1 +1,37 @@
-//codedjkj
+//coded by Manuel Fernández Mercado FCC BUAP 
+
+/*
+Paso 1: obtener los datos del archivo de texto *pendiente*
+
+Paso 2: saber cuantos marcos y páginas hay con su respectivo 
+        tamaño en bits, se obtiene el desplazamiento,
+
+Paso 3: El programa lee un número y saca su numéro de pagina
+
+*/
+
+
+
+/*
+Nota: Todos los tamaños de la memoria están en bytes
+*/
+
+
+struct Memory{
+    physic: u32,
+    virtual: u32,
+    pages: u32,
+    marks: u32,
+}
+
+//important functions using bit manipulation
+
+fn is_power_of_two(n:u32)->bool{
+    return n>0 && (n&(n-1))==0;
+}
+
+
+
+fn main(){
+    
+}
