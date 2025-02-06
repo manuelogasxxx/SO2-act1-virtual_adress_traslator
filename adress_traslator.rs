931bf59ececec7018df749dce4607ae2dbd9ed96
manuelogasxxx <manolito2500604@gmail.com>
2025-02-06 00:29:53 -0600

    masks v1.0

--- adress_traslator.rs
+++ adress_traslator.rs
@@ -43,8 +43,8 @@ fn build_memory(page_size : u32, vm : u32, physic : u32)->Memory{
         page_size,
         pages: vm/page_size,
         frames,
-        page_bits: page_size.leading_zeros(),
-        frame_bits: frames.leading_zeros(),
+        page_bits: page_size.trailing_zeros(),
+        frame_bits: frames.trailing_zeros(),
     }
 }
 
@@ -53,6 +53,24 @@ fn build_memory(page_size : u32, vm : u32, physic : u32)->Memory{
 
 fn is_power_of_two(n:u32)->bool{
     return n>0 && (n&(n-1))==0;
+}
+
+
+
+fn make_low_mask(shift:u32)->u32{
+    if shift>32{
+        panic!("Unable to make that mask :(");
+    }
+    return (1<< shift)-1;
+}
+
+fn make_high_mask(shift:u32)->u32{
+    if shift>32{
+        panic!("Unable to make that mask :(");
+    }
+    let  mut result:u32 = (1<< shift)-1;
+    result<<=shift;
+    return result;
 }
 
 fn memory_check(v: &Vec<u32>)->bool{
@@ -85,6 +103,15 @@ fn read_numbers_from_file(filename: &str) -> io::Result<Vec<u32>> {
 }
 
 
+//función que muestra la información del 
+fn show_info(vm:u32, memory: &Memory){
+    //necesito sacar la información de la memoria
+    println!("jijija");
+    let mut mask1 = make_high_mask(memory.page_bits);
+    println!("{:b}",mask1);
+}
+
+
 fn main(){
    let filename = "pruebita.txt";
    //let numbers = read_numbers_from_file(filename).expect("Error al leer el archivo");
@@ -106,6 +133,27 @@ fn main(){
     println!("{:?}",numbers);
     let mut memory = build_memory(numbers[0],numbers[1],numbers[2]);
     //en este punto ya todo está comprobado y listo
+    //println!("{0}",memory.page_bits);
+    // ya comienzo con la lectura de direcciones virtuales
+    show_info(10, &memory);
+    //no manejo errores cuando ingresa valores no numéricos 
+    /*loop {
+        println!("Ingrese el valor decimal de una dirección virtual");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        // Trim whitespace and handle potential errors
+        let input = input.trim(); // Important: Trim whitespace!
+
+        if input == "salir" {  // Correct comparison
+            break;
+        }
+
+        match input.parse::<u32>() { // More robust parsing
+            Ok(choice) => show_info(choice, &memory),
+            Err(e) => println!("Error: {}", e), // Handle parsing errors
+        }
+    } */
 
     
 
